7f3244ddc6b4557d9d8f746da52382f663f16ea5
suchita rai <suchitai.t.s@gmail.com>
2019-06-11 14:22:32 +0530

    create dbconfig.php having connection and functions

--- dbcon.php
+++ dbcon.php
@@ -1,7 +1,28 @@
 <?php
-define("HOST",'localhost');
-define("ROOT",'root');
-define("PASS",'');
-define("DATABASE",'phptestcode');
-$conn = mysqli_connect(HOST, ROOT, PASS, DATABASE);
+class Dbconfig
+{
+	public $conn;
+
+	public function __construct(){
+		define("HOST",'localhost');
+		define("ROOT",'root');
+		define("PASS",'');
+		define("DATABASE",'phptestcode');
+		$this->conn  = mysqli_connect(HOST, ROOT, PASS, DATABASE);
+	}	
+	
+	public function fetchAllRows($query){
+		$result = mysqli_query($this->conn,$query);
+		return mysqli_fetch_object($result);
+	}
+	
+	public function num_of_rows($query){
+		$result = mysqli_query($this->conn,$query);
+		return mysqli_num_rows($result);
+	}
+	public function query_run($query){
+		$result=mysqli_query($this->conn,$query);
+		return mysqli_affected_rows($result);
+	}
+}
 ?>

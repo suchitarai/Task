5ee89f196406b7710006e5752c3dd986443a33e2
suchita rai <suchitai.t.s@gmail.com>
2019-06-11 17:47:32 +0530

    assign str value and print str value

--- foobar.php
+++ foobar.php
@@ -1,16 +1,25 @@
 <?php
-$str="";
-for($i=1;$i<=100;$i++){
-	if(($i % 3==0) && ($i % 5==0)){
-		$str=$str. "foobar, ";
-	}else if($i % 3==0){
-		$str=$str.  "foo, ";
-	}else if($i % 5==0){
-		$str=$str.  "bar, ";
-	}else{
-		$str=$str.  $i.", ";
+class foobar{
+	public function __construct(){
+	}
+	public function assign_n_display(){
+		$str="";
+		for($i=1;$i<=100;$i++){
+			if(($i % 3==0) && ($i % 5==0)){
+				$str=$str. "foobar, ";
+			}else if($i % 3==0){
+				$str=$str.  "foo, ";
+			}else if($i % 5==0){
+				$str=$str.  "bar, ";
+			}else{
+				$str=$str.  $i.", ";
+			}
+		}
+		$str=rtrim($str,", ");
+		print $str;
 	}
 }
-$str=rtrim($str,", ");
-print $str;
+$obj=new foobar();
+print $obj->assign_n_display();
+
 ?>
